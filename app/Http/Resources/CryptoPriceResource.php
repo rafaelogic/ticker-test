267fb5269fdf94aeb6421aec85c175ac2c11e529
rafaelogic <12.12afa3l@gmail.com>
2023-10-02 12:40:45 +0800

    Add resource to format the api display

---
+++ app/Http/Resources/CryptoPriceResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CryptoPriceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'lowest_market_price'   => $this->lowest_market_price,
+            'lowest_close_time'     => $this->lowest_close_time,
+            'highest_market_price'  => $this->highest_market_price,
+            'highest_close_time'    => $this->highest_close_time
+        ];
+    }
+
+    
+}
